11431d7eb5da206e612a1ac8fc2fb94b43f7877e
bigmike12 <dj.buckwild12@gmail.com>
2022-02-13 20:04:34 +0100

    connecting to heroku server

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { Mongoose } = require("mongoose");
 const connectDB = require("./config/db");
 // const path = require("path");
 
@@ -16,7 +17,14 @@ app.use("/api/auth", require("./routes/auth"));
 app.use("/api/transfer", require("./routes/transaction"));
 // app.use("/api/contacts", require("./routes/contacts"));
 
+Mongoose.connection.on("connected", () => {
+  console.log("Mongoose is connected!!!");
+});
 
 const PORT = process.env.PORT || 5020;
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("build"));
+}
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
